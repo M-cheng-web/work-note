c48b9512b8952fda5817d24328ffbf71ea9e52de
cxh <2604856589@qq.com>
2022-05-16 20:59:32 +0800

    feat: sh start

--- selfEncrypt/start.sh
+++ selfEncrypt/start.sh
@@ -3,4 +3,17 @@
 目的是将脚本隐藏起来,以后直接通过命令来操作文件(也是为了不提交到git上)
 !
 
-echo 123
+
+#!/usr/bin/env sh
+
+echo 1
+
+mv selfEncrypt .selfEncrypt
+
+# filePath=".test_file.zzz"
+# touch $filePath
+# echo "hello liyang" > $filePath
+# echo "文件创建完成"
+# else
+# echo "文件已经存在"
+# fi
