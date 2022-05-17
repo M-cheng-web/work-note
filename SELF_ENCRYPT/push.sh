1fb9bc3f7f308838c4751dac879763ef7a3a7c55
cxh <2604856589@qq.com>
2022-05-17 23:21:29 +0800

    test: 部分更改

--- SELF_ENCRYPT/push.sh
+++ SELF_ENCRYPT/push.sh
@@ -3,13 +3,13 @@
 # 负责提交代码 / 更新代码
 
 defaultBranch=main
+defaultCommit='feat: 每日任务'
 
-# if [ $1 = push ]
-#   then
-#     # git add -A
-#     # git commit -m 'feat: 每日任务'
-#     # git $1 origin $defaultBranch
-# fi
+if [ $1 = push ]
+  then
+    git add -A
+    git commit -m $defaultCommit
+fi
 
 echo git $1 origin $defaultBranch....
 
